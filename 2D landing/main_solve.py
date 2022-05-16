f45f4328efa00d28ec123e0bdb4308d9331e31c0
cuaiswarya <92629414+cuaiswarya@users.noreply.github.com>
2022-05-16 11:41:22 +0200

    Update main_solve.py

--- 2D landing/main_solve.py
+++ 2D landing/main_solve.py
@@ -1,18 +1,15 @@
 from pyrlprob.problem import RLProblem
 from pyrsistent import b
-print('a')
+
 
 # Configuration file
 config_file = "config_files/landing2d.yaml"
-print('b')
 
 
 # Define RL problem
 LandPrb = RLProblem(config_file)
-print('c')
 
 # Solve RL problem
 trainer_dir, exp_dirs, last_cps, best_cp_dir = \
         LandPrb.solve(evaluate=True, postprocess=True, debug=False)
 
-print('d')
